980a3cc7cf682fd4ed274b1dd71b278b80d0f6f3
eriyknas <anastasia9cheshuina@yandex.ru>
2026-04-01 10:26:25 +0300

    Destructuring in JS has been studied

--- js/main.js
+++ js/main.js
@@ -9,19 +9,45 @@
 // console.log(c4);
 
 
-console.log("Деструктуризация в параметрах");
-console.log("Деструктуризация объектов");
-const user = {
-    name: "Алиса",
-    age: 25,
-    city: "Москва",
-}
-const userName = user.name;
-const userAge = user.age;
-console.log(userName, userAge);
-const {name, age , city } = user;
-console.log(name,age, city);
-const {name: fullName, age: years} = user;
-console.log(fullName, years);
-const {name: personName, country = "Россия"} = user;
-console.log(personName, country);
+// console.log("Деструктуризация в параметрах");
+// console.log("Деструктуризация объектов");
+// const user = {
+//     name: "Алиса",
+//     age: 25,
+//     city: "Москва",
+// }
+// function printUserOld({name, age, city}) {
+//     console.log(`Имя: ${name}`);
+//     console.log(`Возраст: ${age}`);
+//     console.log(`Город: ${city}`);
+// }
+// printUserOld(user);
+// console.log();
+// printUserOld(user);
+// const userName = user.name;
+// const userAge = user.age;
+// console.log(userName, userAge);
+// const {name, age , city } = user;
+// console.log(name,age, city);
+// const {name: fullName, age: years} = user;
+// console.log(fullName, years);
+// const {name: personName, country = "Россия"} = user;
+// console.log(personName, country);
+
+//6.5 Практика продукты
+// console.log("Продукты");
+// const product = {
+//     name: "Картошка",
+//     price: 119,
+//     category: "Беларусский",
+//     inStock: 12,
+// }
+// function printProduct({name,price, category, inStock}) {
+//     console.log(`Имя: ${name}`);
+//     console.log(`Цена: ${price}`);
+//     console.log(`Категория: ${category}`);
+//     console.log(`Наличие: ${inStock}`);
+// }
+// printProduct(product);
+// console.log();
+// printProduct(product);
